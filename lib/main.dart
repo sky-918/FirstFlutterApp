3be47c2a1e78d0cb618c9a00c0e33fa0ee3f0701
tianyangyang <tianyangyang@mysteel.com>
2021-04-02 17:12:57 +0800

    feat:1,基础控件页面添加

--- lib/main.dart
+++ lib/main.dart
@@ -77,6 +77,18 @@ class MyApp extends StatelessWidget {
         },
         "counter": (context) {
           return CounterWidget();
+        },
+        "TapboxA": (context) {
+          return TapboxA();
+        },
+        "ParentWidget": (context) {
+          return ParentWidget();
+        },
+        "ParentWidgetC": (context) {
+          return ParentWidgetC();
+        },
+        "BaseView": (context) {
+          return BaseView();
         }
       },
     );
@@ -189,6 +201,27 @@ class _MyHomePageState extends State<MyHomePage> {
                 print("路由返回值：$result");
               },
               child: Text("打开提示页面"),
+            ),
+            FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed("TapboxA");
+                },
+                child: Text("测试Widget状态")),
+            FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed("ParentWidget");
+                },
+                child: Text("测试父管理子的Widget状态")),
+            FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed("ParentWidgetC");
+                },
+                child: Text("测试混合管理Widget状态")),
+            RaisedButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed("BaseView");
+              },
+              child: Text("基础view"),
             ),
             RandomWordsWidget(),
           ],
@@ -358,6 +391,187 @@ class _TapboxAState extends State<TapboxA> {
         height: 200.0,
         decoration: new BoxDecoration(
           color: _active ? Colors.lightBlue[700] : Colors.green[600],
+        ),
+      ),
+    );
+  }
+}
+
+// ParentWidget 为 TapboxB 管理状态.
+
+//------------------------ ParentWidget --------------------------------
+
+class ParentWidget extends StatefulWidget {
+  @override
+  _ParentWidgetState createState() => new _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: new TapboxB(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+//------------------------- TapboxB ----------------------------------
+
+class TapboxB extends StatelessWidget {
+  TapboxB({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+  Widget build(BuildContext context) {
+    return new GestureDetector(
+      onTap: _handleTap,
+      child: new Container(
+        child: new Center(
+          child: new Text(
+            active ? 'Active' : 'Inactive',
+            style: new TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: new BoxDecoration(
+          color: active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+      ),
+    );
+  }
+}
+
+//---------------------------- ParentWidget ----------------------------
+
+class ParentWidgetC extends StatefulWidget {
+  @override
+  _ParentWidgetCState createState() => new _ParentWidgetCState();
+}
+
+class _ParentWidgetCState extends State<ParentWidgetC> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: new TapboxC(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+//----------------------------- TapboxC ------------------------------
+
+class TapboxC extends StatefulWidget {
+  TapboxC({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  @override
+  _TapboxCState createState() => new _TapboxCState();
+}
+
+class _TapboxCState extends State<TapboxC> {
+  bool _highlight = false;
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _highlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTap() {
+    widget.onChanged(!widget.active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // 在按下时添加绿色边框，当抬起时，取消高亮
+    return new GestureDetector(
+      onTapDown: _handleTapDown,
+      // 处理按下事件
+      onTapUp: _handleTapUp,
+      // 处理抬起事件
+      onTap: _handleTap,
+      onTapCancel: _handleTapCancel,
+      child: new Container(
+        child: new Center(
+          child: new Text(widget.active ? 'Active' : 'Inactive',
+              style: new TextStyle(fontSize: 32.0, color: Colors.white)),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: new BoxDecoration(
+          color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+          border: _highlight
+              ? new Border.all(
+                  color: Colors.teal[700],
+                  width: 10.0,
+                )
+              : null,
+        ),
+      ),
+    );
+  }
+}
+
+class BaseView extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return ImageStates();
+  }
+}
+
+class ImageStates extends State {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("基础控件"),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[Text("标题")],
         ),
       ),
     );
