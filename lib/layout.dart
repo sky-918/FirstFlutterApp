2f21985c15ea005f360ed2b689ca45db74b41c84
tianyangyang <tianyangyang@mysteel.com>
2021-06-17 19:40:31 +0800

    feat:1.增加了简单对话框在colorThem页面中

--- lib/layout.dart
+++ lib/layout.dart
@@ -740,6 +740,147 @@ class _ClorAndThem extends State<ColorAndThem> {
   @override
   Widget build(BuildContext context) {
     ThemeData themeData = Theme.of(context);
+
+// 弹出对话框
+    Future<bool> showDeleteConfirmDialog1() {
+      return showDialog<bool>(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("提示"),
+            content: Text("您确定要删除当前文件吗?"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("取消",),color: Colors.red,
+                onPressed: () => Navigator.of(context).pop(), // 关闭对话框
+              ),
+              FlatButton(
+                child: Text("删除"),
+                onPressed: () {
+                  //关闭对话框并返回true
+                  Navigator.of(context).pop(true);
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+    Future<void> changeLanguage() async {
+      int i = await showDialog<int>(
+          context: context,
+          builder: (BuildContext context) {
+            return SimpleDialog(
+              title: const Text('请选择语言'),
+              children: <Widget>[
+                SimpleDialogOption(
+                  onPressed: () {
+                    // 返回1
+                    Navigator.pop(context, 1);
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 6),
+                    child: const Text('中文简体'),
+                  ),
+                ),
+                SimpleDialogOption(
+                  onPressed: () {
+                    // 返回2
+                    Navigator.pop(context, 2);
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 6),
+                    child: const Text('美国英语'),
+                  ),
+                ),
+              ],
+            );
+          });
+
+      if (i != null) {
+        print("选择了：${i == 1 ? "中文简体" : "美国英语"}");
+      }
+    }
+    Future<void> showListDialog() async {
+      int index = await showDialog<int>(
+        context: context,
+        builder: (BuildContext context) {
+          var child = Column(
+            children: <Widget>[
+              ListTile(title: Text("请选择")),
+              Expanded(
+                  child: ListView.builder(
+                    itemCount: 30,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListTile(
+                        title: Text("$index"),
+                        onTap: () => Navigator.of(context).pop(index),
+                      );
+                    },
+                  )),
+            ],
+          );
+          //使用AlertDialog会报错
+          //return AlertDialog(content: child);
+          return Dialog(child: child);
+        },
+      );
+      if (index != null) {
+        print("点击了：$index");
+      }
+    }
+    Future<T> showCustomDialog<T>({
+      @required BuildContext context,
+      bool barrierDismissible = true,
+      WidgetBuilder builder,
+    }) {
+      final ThemeData theme = Theme.of(context,);
+      return showGeneralDialog(
+        context: context,
+        pageBuilder: (BuildContext buildContext, Animation<double> animation,
+            Animation<double> secondaryAnimation) {
+          final Widget pageChild = Builder(builder: builder);
+          return SafeArea(
+            child: Builder(builder: (BuildContext context) {
+              return theme != null
+                  ? Theme(data: theme, child: pageChild)
+                  : pageChild;
+            }),
+          );
+        },
+        barrierDismissible: barrierDismissible,
+        barrierLabel: MaterialLocalizations.of(context).modalBarrierDismissLabel,
+        barrierColor: Colors.black87, // 自定义遮罩颜色
+        transitionDuration: const Duration(milliseconds: 150),
+        transitionBuilder: _buildMaterialDialogTransitions,
+      );
+    }
+
+
+    Future<bool> showDeleteConfirmDialog2() {
+      return   showCustomDialog<bool>(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("提示"),
+            content: Text("您确定要删除当前文件吗?"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("取消"),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+              FlatButton(
+                child: Text("删除"),
+                onPressed: () {
+                  // 执行删除操作
+                  Navigator.of(context).pop(true);
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
     return Theme(
         data: ThemeData(
             primarySwatch: _themeColor, //用于导航栏、FloatingActionButton的背景色等
@@ -772,6 +913,40 @@ class _ClorAndThem extends State<ColorAndThem> {
                     Text("  颜色固定黑色")
                   ]),
             ),
+            RaisedButton(
+              child: Text("对话框1"),
+              onPressed: () async {
+                //弹出对话框并等待其关闭
+                bool delete = await showDeleteConfirmDialog1();
+                if (delete == null) {
+                  print("取消删除");
+                } else {
+                  print("已确认删除");
+                  //... 删除文件
+                }
+              },
+            ),
+            RaisedButton(
+              child: Text("对话框2"),
+              onPressed: () async {
+                //弹出对话框并等待其关闭
+                changeLanguage();
+              },
+            ),
+            RaisedButton(
+              child: Text("对话框3"),
+              onPressed: () async {
+                //弹出对话框并等待其关闭
+                showListDialog();
+              },
+            ),
+            RaisedButton(
+              child: Text("自定义"),
+              onPressed: () async {
+                //弹出对话框并等待其关闭
+                showDeleteConfirmDialog2();
+              },
+            ),
           ]),
           floatingActionButton: FloatingActionButton(
               onPressed: () => //切换主题
@@ -782,6 +957,20 @@ class _ClorAndThem extends State<ColorAndThem> {
   }
 }
 
+Widget _buildMaterialDialogTransitions(
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child) {
+  // 使用缩放动画
+  return ScaleTransition(
+    scale: CurvedAnimation(
+      parent: animation,
+      curve: Curves.easeOut,
+    ),
+    child: child,
+  );
+}
 class NavBar extends StatelessWidget {
   final String title;
   final Color color; //背景颜色
