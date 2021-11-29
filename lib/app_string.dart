c2522f70989545c2140905979d8dce5612474372
tianyangyang <1004994051@qq.com>
2021-11-29 16:26:57 +0800

    refactor:创建字符串常量

---
+++ lib/app_string.dart
@@ -0,0 +1,30 @@
+/// @auter Created by tyy on 2021/11/29
+/// desc   :
+/// version:
+class AppString {
+  static const List<String> menuTitle = ['头条', '咨询', '短信', "供求", '期货'];
+  static const List<String> menuTitleIcon = [
+    'https://img2.baidu.com/it/u=3432174479,1450323813&fm=26&fmt=auto',
+    'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fup.enterdesk.com%2Fedpic%2F6e%2F25%2F98%2F6e25987277d05dae9643dcee6ec020ec.jpg&refer=http%3A%2F%2Fup.enterdesk.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1640764418&t=f14f7a54c86a41ec076bb852fc04c199',
+    'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fup.enterdesk.com%2Fedpic_source%2Fc5%2Fdb%2Fa1%2Fc5dba1440ec61c8041cdfb16bbbc0de2.jpeg&refer=http%3A%2F%2Fup.enterdesk.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1640764418&t=341c192bad1927960234787c4a35dedb',
+    "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fup.enterdesk.com%2Fedpic%2F78%2Fe0%2Fe4%2F78e0e4db3a3a679fc5647ae2ef4379c7.jpeg&refer=http%3A%2F%2Fup.enterdesk.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1640764418&t=8f6ab8758c691d32c574c46ab0dffab1",
+    'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fup.enterdesk.com%2Fedpic%2Ffd%2Ff1%2Fda%2Ffdf1dacb8ff0b8f13ed29bcbee42f328.jpeg&refer=http%3A%2F%2Fup.enterdesk.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1640764418&t=3369e059377b0795831069671e89fa35'
+  ];
+  static const titleArray = [
+    "头条",
+    "黑色金属",
+    "镍铬不锈钢",
+    "建筑材料",
+    "有色金属",
+    "农产品",
+    "黑色金属",
+    "能源化工"
+  ];
+  static const imgList = [
+    'https://file02.16sucai.com/d/file/2014/0829/b871e1addf5f8e96f3b390ece2b2da0d.jpg',
+    'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F2019-08-28%2F5d6649239aed2.jpg&refer=http%3A%2F%2Fpic1.win4000.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1640765799&t=d72568fbd902e55cffd8b690a4ca62aa',
+    'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201504%2F17%2F20150417H1705_AYwKF.jpeg&refer=http%3A%2F%2Fb-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1640765799&t=cb439c880b3f9a946a78a2cf8b61c331',
+    'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F2018-11-19%2F5bf2563439059.jpg&refer=http%3A%2F%2Fpic1.win4000.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1640765799&t=16cd8c3d25d0a3edba01b7c4303703f5',
+    'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2Fdd502cc56049fca09420e7b77a786bc5c7a9b3ffb1371-l7vKt5_fw658&refer=http%3A%2F%2Fhbimg.b0.upaiyun.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1640765799&t=92e023d3cc5dd3da1cfac9a77c53f2c4'
+  ];
+}
--- lib/steel_home.dart
+++ lib/steel_home.dart
@@ -1,3 +1,4 @@
+import 'package:first_flutter_app/app_string.dart';
 import 'package:first_flutter_app/mybanner/page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -18,31 +19,6 @@ class SteelHome extends StatefulWidget {
 }
 
 class _SteelHomeState extends State<SteelHome> {
-  var _color = Colors.black;
-  var _position = 0;
-  final List<String> _menuTitle = ['头条', '咨询', '短信', "供求",'期货'];
-  final List<String> _menuTitleIcon = ['https://img2.baidu.com/it/u=3432174479,1450323813&fm=26&fmt=auto',
-    'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fup.enterdesk.com%2Fedpic%2F6e%2F25%2F98%2F6e25987277d05dae9643dcee6ec020ec.jpg&refer=http%3A%2F%2Fup.enterdesk.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1640764418&t=f14f7a54c86a41ec076bb852fc04c199',
-    'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fup.enterdesk.com%2Fedpic_source%2Fc5%2Fdb%2Fa1%2Fc5dba1440ec61c8041cdfb16bbbc0de2.jpeg&refer=http%3A%2F%2Fup.enterdesk.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1640764418&t=341c192bad1927960234787c4a35dedb',
-    "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fup.enterdesk.com%2Fedpic%2F78%2Fe0%2Fe4%2F78e0e4db3a3a679fc5647ae2ef4379c7.jpeg&refer=http%3A%2F%2Fup.enterdesk.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1640764418&t=8f6ab8758c691d32c574c46ab0dffab1",
-    'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fup.enterdesk.com%2Fedpic%2Ffd%2Ff1%2Fda%2Ffdf1dacb8ff0b8f13ed29bcbee42f328.jpeg&refer=http%3A%2F%2Fup.enterdesk.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1640764418&t=3369e059377b0795831069671e89fa35'];
-  var _titleArray = [
-    "头条",
-    "黑色金属",
-    "镍铬不锈钢",
-    "建筑材料",
-    "有色金属",
-    "农产品",
-    "黑色金属",
-    "能源化工"
-  ];
-  var _imgList = [
-    'https://file02.16sucai.com/d/file/2014/0829/b871e1addf5f8e96f3b390ece2b2da0d.jpg',
-    'https://file02.16sucai.com/d/file/2014/0829/b871e1addf5f8e96f3b390ece2b2da0d.jpg',
-    'https://file02.16sucai.com/d/file/2014/0829/b871e1addf5f8e96f3b390ece2b2da0d.jpg',
-    'https://file02.16sucai.com/d/file/2014/0829/b871e1addf5f8e96f3b390ece2b2da0d.jpg',
-    'https://file02.16sucai.com/d/file/2014/0829/b871e1addf5f8e96f3b390ece2b2da0d.jpg'
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -61,11 +37,11 @@ class _SteelHomeState extends State<SteelHome> {
         children: [
           // getSingleChildScrollView(),
           // getRefreshIndicator(),
-          TitleList(list: _titleArray),
-          SteelBanner(imgList: _imgList,),
+          TitleList(list: AppString.titleArray),
+          SteelBanner(imgList: AppString.imgList,),
           SizedBox(height: 5,),
-          GridViewMenu(color: Colors.transparent,data: _menuTitle,icons: _menuTitleIcon,onItemClick: (inedex){
-            showToast(_menuTitle[inedex]);
+          GridViewMenu(color: Colors.transparent,data:  AppString.menuTitle,icons:  AppString.menuTitleIcon,onItemClick: (inedex){
+            showToast( AppString.menuTitle[inedex]);
           },),
         ],
       ),
